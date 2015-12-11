c37d57301b397da04a28ef9d319ca07e22d8776d
稿作 <kousaku@FUNAICT201314-no-MacBook-Air.local>
2015-12-11 12:02:27 +0900

    マイページのレイアウト作成しました

---
+++ makeShare/MypageViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MypageViewController.swift
+//  makeShare
+//
+//  Created by 稿作 on 2015/12/11.
+//  Copyright © 2015年 JKCommittee. All rights reserved.
+//
+
+import UIKit
+
+class MypageViewController: UIViewController {
+
+   
+}
